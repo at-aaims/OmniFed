96183dbb446c529d1b2aa8c49265ba8825b11b27
sahiltyagi4 <sahilt.tyagi@gmail.com>
2025-07-17 13:53:41 -0400

    running models with different FL algos

--- test_scripts/iid_vgg11.sh
+++ test_scripts/iid_vgg11.sh
@@ -5,10 +5,12 @@ cd ../
 # kill -s 9 `ps -ef | grep src.flora.test.launch_training |grep -v grep | awk '{print $2}'`
 # kill -9 $(ps aux | grep src.flora.test.launch_training | grep -v grep | awk '{print $2}')
 
-dir='/Users/ssq/Desktop/datasets/flora_test/'
+#dir='/Users/ssq/Desktop/datasets/flora_test/'
+dir='/ccsopen/home/ssq/datasets/'
+interface='eth1'
 bsz=32
 testbsz=32
-worldsize=4
+worldsize=8
 #comm='RPC'
 comm='Collective'
 algo='fedavg'
@@ -16,7 +18,7 @@ commfreq=10
 masteraddr='127.0.0.1'
 masterport=28670
 #masterport=50055
-backend='Gloo'
+backend='gloo'
 lr=0.01
 gamma=0.2
 momentum=0.9
@@ -31,7 +33,7 @@ do
   python3 -m src.flora.test.launch_training --dir=$dir --bsz=$bsz --rank=$rank --world-size=$worldsize \
   --communicator=$comm --comm-freq=$commfreq --master-addr=$masteraddr --master-port=$masterport --backend=$backend \
   --model=$model --dataset=$dataset --train-dir=$dir --test-dir=$dir --algo=$algo --lr=$lr --gamma=$gamma \
-  --weight-decay=$weightdecay --momentum=$momentum --test-bsz=$testbsz &
-  echo "going to sleep for 3 seconds..."
+  --weight-decay=$weightdecay --momentum=$momentum --test-bsz=$testbsz --network-interface=$interface &
+  echo "going to sleep for 2 seconds..."
   sleep 3
 done
