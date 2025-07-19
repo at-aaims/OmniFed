dbafb8f817a9df3c78a397a0034c449c166cb5ae
sahiltyagi4 <sahilt.tyagi@gmail.com>
2025-07-19 00:34:15 -0400

    running models with different FL algos

--- src/flora/test/train_model.py
+++ src/flora/test/train_model.py
@@ -346,7 +346,9 @@ class ModelTrainer(object):
                 # for ResNet18 CIFAR10
                 # personal_head=SimpleFedPerHead(input_dim=1000, num_classes=10),
                 # for VGG11 CIFAR100
-                personal_head=SimpleFedPerHead(input_dim=1000, num_classes=100),
+                # personal_head=SimpleFedPerHead(input_dim=1000, num_classes=100),
+                # for AlexNet
+                personal_head=SimpleFedPerHead(input_dim=1000, num_classes=101),
                 train_data=self.train_dataloader,
                 test_data=self.test_dataloader,
                 communicator=self.communicator,
