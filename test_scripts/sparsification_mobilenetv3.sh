1b20eea3797943f739a1b6380b3bb515e1ce341f
sahiltyagi4 <sahilt.tyagi@gmail.com>
2025-07-22 14:30:15 -0400

    running models with different compression schemes

--- test_scripts/sparsification_mobilenetv3.sh
+++ test_scripts/sparsification_mobilenetv3.sh
@@ -5,13 +5,25 @@ cd ../
 # kill -s 9 `ps -ef | grep src.flora.test.launch_sparsification |grep -v grep | awk '{print $2}'`
 # kill -9 $(ps aux | grep src.flora.test.launch_sparsification | grep -v grep | awk '{print $2}')
 
-dir='/Users/ssq/Desktop/datasets/flora_test/'
+#dir='/Users/ssq/Desktop/datasets/flora_test/'
+#worldsize=4
+#interface='lo0'
+#masterport=28670
+
+#dir='/ccsopen/home/ssq/datasets/'
+#masterport=24870
+#dir='/ccsopen/home/ssq/datasets2/'
+#masterport=29860
+dir='/ccsopen/home/ssq/datasets3/'
+masterport=23450
+#dir='/ccsopen/home/ssq/datasets4/'
+#masterport=26290
+worldsize=4
+interface='eth1'
 bsz=32
 testbsz=32
-worldsize=4
 masteraddr='127.0.0.1'
-masterport=28670
-backend='Gloo'
+backend='gloo'
 model='mobilenetv3'
 dataset='caltech256'
 lr=0.1
@@ -20,7 +32,7 @@ weightdecay=1e-4
 momentum=0.9
 lrstepsize=40
 numclasses=257
-compression='randomK'
+compression='topK'
 compressratio=0.1
 
 for val in $(seq 1 $worldsize)
@@ -31,6 +43,6 @@ do
   --master-addr=$masteraddr --master-port=$masterport --backend=$backend --model=$model --dataset=$dataset \
   --train-dir=$dir --test-dir=$dir --compression-type=$compression --compress-ratio=$compressratio --lr=$lr \
   --gamma=$gamma --weight-decay=$weightdecay --momentum=$momentum --test-bsz=$testbsz \
-  --mobv3-lr-step-size=$lrstepsize --mobv3-num-classes=$numclasses &
+  --mobv3-lr-step-size=$lrstepsize --mobv3-num-classes=$numclasses --network-interface=$interface &
   sleep 3
 done
