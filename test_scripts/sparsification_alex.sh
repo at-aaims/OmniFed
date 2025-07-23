20593871c47062cf552dd279c81a74c480d45d8f
sahiltyagi4 <sahilt.tyagi@gmail.com>
2025-07-23 09:36:36 -0400

    running models with different compression schemes

--- test_scripts/sparsification_alex.sh
+++ test_scripts/sparsification_alex.sh
@@ -14,10 +14,10 @@ cd ../
 #masterport=29110
 #dir='/ccsopen/home/ssq/datasets2/'
 #masterport=29118
-dir='/ccsopen/home/ssq/datasets3/'
-masterport=23457
-#dir='/ccsopen/home/ssq/datasets4/'
-#masterport=26290
+#dir='/ccsopen/home/ssq/datasets3/'
+#masterport=23457
+dir='/ccsopen/home/ssq/datasets4/'
+masterport=26884
 interface='eth1'
 worldsize=8
 bsz=32
@@ -30,8 +30,8 @@ lr=0.01
 gamma=0.1
 weightdecay=5e-4
 momentum=0.9
-compression='topK'
-compressratio=0.001
+compression='dgc'
+compressratio=0.1
 
 for val in $(seq 1 $worldsize)
 do
