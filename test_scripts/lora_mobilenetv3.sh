b227cb9b261fa9249128c62ef6313e606cadf1cf
Sahil Tyagi <styagi@iu.edu>
2025-07-31 03:22:25 -0400

    running powersgd

--- test_scripts/lora_mobilenetv3.sh
+++ test_scripts/lora_mobilenetv3.sh
@@ -31,8 +31,8 @@ compression='PowerSGD'
 poweritr=5
 #compressrank=64
 #mincompressrate=20
-compressrank=16
-mincompressrate=10
+compressrank=32
+mincompressrate=20
 
 for val in $(seq 1 $worldsize)
 do
