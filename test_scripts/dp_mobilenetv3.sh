439c39da7611582178238885f8666befcdf891a5
Sahil Tyagi <styagi@iu.edu>
2025-08-05 22:27:25 -0400

    diff privacy

--- test_scripts/dp_mobilenetv3.sh
+++ test_scripts/dp_mobilenetv3.sh
@@ -31,13 +31,13 @@ lrstepsize=40
 numclasses=257
 epochs=80
 
-epsilon=1.0
+#epsilon=1.0
+#delta=1e-5
+#gamma=0.01
+
+epsilon=10.0
 delta=1e-5
 gamma=0.01
-
-#epsilon=20.0
-#delta=2e-5
-#gamma=0.1
 
 for val in $(seq 1 $worldsize)
 do
